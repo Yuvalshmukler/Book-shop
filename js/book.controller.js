7e082b7c6bb0c00e35abf449ec8fdd64f2527bb3
Yuval <yuval.shmukler@gmail.com>
2022-06-07 14:06:04 +0300

    first commit

---
+++ js/book.controller.js
@@ -0,0 +1,117 @@
+'use strict'
+
+var gCurrBook
+
+
+function onInit() {
+    renderBooks()
+    renderFilterByQueryStringParams()
+    restartGame()
+}
+
+function renderBooks() {
+    var books = getBooks()
+    console.log(books);
+    var strHTML = books.map(
+        (book) => `
+       <tr>
+           <td>${book.id}</td>
+           <td>${book.title}</td>
+           <td>${book.price}$</td>
+           <td>
+               <button onClick="onReadBook('${book.id}')"class="action-read">Read</button>
+               <button onClick="onUpdateBook('${book.id}')"class="action-update">Update</button>
+               <button onClick="onDeleteBook('${book.id}')"class="action-delete">Delete</button>
+           </td>
+       </tr>
+
+`
+    )
+    
+    document.querySelector('tbody').innerHTML = strHTML.join('')
+}
+
+function onReadBook(bookId) {
+    var book = getBookById(bookId)
+    var elModal = document.querySelector('.modal')
+    elModal.querySelector('h3').innerText = book.title
+    elModal.querySelector('h4 span').innerText = book.price
+    elModal.querySelector('p').innerText = book.desc
+    elModal.classList.add('open')
+    elModal.dataset.bookId = bookId
+    //console.log(elModal.dataset);
+    //gCurrBook = book
+    var elRateDisplay = document.querySelector('.onRange')
+    elRateDisplay.innerText = book.rate
+
+}
+
+
+function onCloseModal() {
+    document.querySelector('.modal').classList.remove('open')
+    var elRangeBtn = document.querySelector('.onRange')
+    elRangeBtn.innerText = 0
+}
+function onDeleteBook(bookId) {
+    deleteBookById(bookId)
+    renderBooks()
+
+}
+function onAddBook() {
+    var title = prompt('Book title')
+    console.log(title);
+    var price = prompt('price?')
+    if (title) {
+        const book = addBook(title, price)
+        renderBooks()
+    }
+}
+function onUpdateBook(bookId) {
+    var newPrice = prompt('Enter a new price')
+    if (!newPrice) return
+    newPrice += '$'
+    updateBook(bookId, newPrice)
+    renderBooks()
+}
+
+function onRange(diff) {
+    //console.log(diff);
+    var elRateDisplay = document.querySelector('.onRange')
+    var elModal = document.querySelector('.modal')
+
+    const book = getBookById(elModal.dataset.bookId)
+    if (+elRateDisplay.innerText === 10 && diff === 1) return
+    if (+elRateDisplay.innerText === 0 && diff === -1) return
+
+    setRate(book, diff)
+    elRateDisplay.innerText = book.rate
+}
+
+function onSetFilterBy(filterBy) {
+    filterBy = setFilterBy(filterBy)
+    renderBooks()
+
+    const queryStringParams = `?price=${filterBy.maxPrice}&rate=${filterBy.minRate}&txt=${filterBy.txt}`
+    const newUrl = window.location.protocol + '//' + window.location.host + window.location.pathname + queryStringParams
+    window.history.pushState({ path: newUrl }, '', newUrl)
+
+}
+
+function renderFilterByQueryStringParams() {
+    const queryStringParams = new URLSearchParams(window.location.search)
+    const filterBy = {
+        maxPrice: +queryStringParams.get('price') || 0,
+        minRate: +queryStringParams.get('rate') || 0,
+        txt: queryStringParams.get('txt') || '',
+    }
+
+
+    if (!filterBy.txt && !filterBy.maxPrice && filterBy.minRate) return
+    onSetFilterBy(filterBy)
+
+}
+
+function onNextPage() {
+    nextPage()
+    renderBooks()
+}
---
+++ js/services/book.service.js
@@ -0,0 +1,152 @@
+'use strict'
+
+
+const gTitles = ['Learning Laravel', 'Beginning with Laravel', 'Java for dev']
+const STORAGE_KEY = 'bookDB'
+const PAGE_SIZE = 5
+var gBooks
+var gId = 0
+var gPageIdx = 0
+var gFilterBy = {
+    maxPrice: 0,
+    minRate: 0,
+    txt: ''
+}
+
+_createBooks()
+
+
+function restartGame(){
+    gFilterBy = {
+        maxPrice: 0,
+        minRate: 0,
+        txt: ''
+    }
+    gPageIdx = 0
+    gId = 0
+
+}
+
+
+function getBooks() {
+    var books = getBooksForDisplay()
+    const startIdx = gPageIdx * PAGE_SIZE
+    books = books.slice(startIdx, startIdx + PAGE_SIZE)
+
+    return books
+}
+
+function getBooksForDisplay() {
+    var books = gBooks
+    //if (!gFilterBy) return books
+
+    if (gFilterBy.txt) {
+        books = books.filter(book => book.title.includes(gFilterBy.txt))
+    }
+    if (gFilterBy.minRate) { 
+        books = books.filter(book => book.rate >= +gFilterBy.minRate)
+        console.log(books);
+
+    }
+    if (gFilterBy.maxPrice) {
+        books = books.filter(book => book.price <= +gFilterBy.maxPrice)
+        console.log(books);
+    }
+    return books
+}
+
+/* function setFilterByMax(filterby) {
+    gFilterBy.maxPrice = filterby
+    console.log(filterby)
+
+}
+function getFilterByRate(minRate) {
+    console.log(minRate)
+    gFilterBy.minRate = minRate
+}
+ */
+
+function setFilterBy(filterBy) {
+    if(filterBy.maxPrice) gFilterBy.maxPrice = +filterBy.maxPrice
+    if(filterBy.minRate) gFilterBy.minRate = +filterBy.minRate
+    if(filterBy.txt) gFilterBy.txt = filterBy.txt
+    return gFilterBy
+}
+function _createBook(title = makeLorem(4)) {
+    return {
+        id: makeId(2),
+        desc: makeLorem(90),
+        title,
+        price: getRandomIntInclusive(1, 25),
+        rate: 0
+    }
+}
+
+
+function getTitles() {
+    return gTitles
+}
+
+function _createBooks() {
+    var books = loadFromStorage(STORAGE_KEY)
+
+    if (!books || !books.length) {
+        books = []
+        for (let i = 0; i < 15; i++) {
+            books.push(_createBook())
+        }
+    }
+    gBooks = books
+    console.log(gBooks);
+    _saveBooksToStorage()
+}
+
+
+function getBookById(bookId) {
+    const book = gBooks.find((book) => bookId === book.id)
+    return book
+}
+
+function deleteBookById(bookId) {
+    const bookIdx = gBooks.findIndex((book) => bookId === book.id)
+    gBooks.splice(bookIdx, 1)
+    _saveBooksToStorage()
+
+}
+/* function updateBook(bookId){
+    const book = gBook.find(book =>  )
+}
+ */
+function addBook(title, price) {
+    const book = _createBook(title, price)
+    console.log(book);
+    gBooks.push(book)
+    console.log(gBooks);
+    _saveBooksToStorage()
+    return book
+}
+
+function updateBook(bookId, newPrice) {
+    const book = gBooks.find(book => book.id === bookId)
+    book.price = newPrice
+    _saveBooksToStorage()
+    return book
+
+}
+
+function _saveBooksToStorage() {
+    saveToStorage(STORAGE_KEY, gBooks)
+}
+
+
+function setRate(book, diff) {
+    book.rate += diff
+    _saveBooksToStorage()
+}
+
+function nextPage() {
+    gPageIdx++
+    if (gPageIdx * PAGE_SIZE >= gBooks.length) gPageIdx = 0
+}
+
+///////////////////////////////////////////////
